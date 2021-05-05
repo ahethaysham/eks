79d653b7e4662d1b6437660de574931a7243d0fd
Ahethaysham Ahmed <ahethaysham.ahmed@mphasis.com>
2021-05-05 22:05:41 +0530

    add

---
+++ environment/dev/eksctl.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+PRIVATE_SUBNET_1=`terraform output private-subnets-ids | sed -n 2p | cut -d'"' -f2`
+PRIVATE_SUBNET_2=`terraform output private-subnets-ids | sed -n 3p | cut -d'"' -f2`
+
+
+PUBLIC_SUBNET_1=`terraform output public-subnet-ids | sed -n 2p | cut -d'"' -f2`
+PUBLIC_SUBNET_2=`terraform output public-subnet-ids | sed -n 3p | cut -d'"' -f2`
+
+KEY_NAME="mphasisalta-ca-eks"
+
+
+
+
+# Creating a key pair for EC2 Workers Nodes
+
+mkdir ~/.ssh 2>&1 >/dev/null
+
+aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > ~/.ssh/$KEY_NAME.pem
+
+
+eksctl create cluster \
+  --name mphasisalta-ca-eks \
+  --version 1.19 \
+  --vpc-private-subnets=$PRIVATE_SUBNET_1,$PRIVATE_SUBNET_2 \
+  --vpc-public-subnets=$PUBLIC_SUBNET_1,$PUBLIC_SUBNET_2 \
+  --region ap-south-1 \
+  --nodegroup-name worker \
+  --node-type t3a.medium \
+  --nodes 2 \
+  --nodes-min 1 \
+  --nodes-max 4 \
+  --ssh-access \
+  --node-volume-size 30 \
+  --ssh-public-key $KEY_NAME \
+  --appmesh-access \
+  --full-ecr-access \
+  --alb-ingress-access \
+  --managed \
+  --asg-access \
+  --verbose 3
+  
+  
+
+
+
+# UPDATE YOUR ./kube
+################################################################
+### MUST ###
+###---> aws eks update-kubeconfig --name mphasisalta-ca-eks --region us-east-1 <---
+##################################################################
+
+# Update Public to Private End Points
+# aws eks update-cluster-config --name mphasisalta-ca-eks --region us-east-1 \
+# --resources-vpc-config endpointPublicAccess=false,endpointPrivateAccess=true
+
+# https://www.stacksimplify.com/aws-eks/aws-loadbalancers/aws-eks-create-private-nodegroup/
+
+#END
